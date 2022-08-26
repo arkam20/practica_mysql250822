4fafdb075904dad3bd5f3b1c1001f34224ea4157
sergio patricio <soporte.tzone@gmail.com>
2022-08-25 19:14:12 -0600

    configuracion basica server
    Se establece el arranque, puerto y turas basicas para que el server funcione

---
+++ app.js
@@ -0,0 +1,19 @@
+//Se establece la ruta para variables de entorno
+require('dotenv').config({path:'env/.env'});
+
+const express = require('express');
+const app = express();
+
+const srv_port = process.env.SERVER_PORT;
+
+
+//Rutas
+
+app.get('/', ( req , res )=>{
+    res.send('Hola cucho');
+});
+
+app.listen(srv_port,() => {
+    console.log('Servidor iniciado en http://localhost:',srv_port);
+});
+
